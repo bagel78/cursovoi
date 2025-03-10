2d971ac0e03133735b5b2a5a3d2e21b8cd857274
Aleksandra <39363016+bagel78@users.noreply.github.com>
2025-03-10 23:20:04 +0700

    Delete Project_1/cursovoi directory

--- Project_1/cursovoi/database/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.database, name='database'),
-    path('create', views.create, name='create')
-]
-
--- Project_1/cursovoi/database/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Students
-
-def database(request):
-    student = Students.objects.order_by('last_name') # сортировка по фамилии
-    return render(request, 'database/database.html', {'student': student})
-
-def create(request):
-    return render(request, 'database/create.html')
